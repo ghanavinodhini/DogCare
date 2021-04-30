bc0069e9b692a89a7a64c353f8d7cf25af9a9f49
ghanavinodhini <ghanacvino@gmail.com>
2021-04-30 19:00:09 +0530

    Router Link changes

--- src/toolbar/Toolbar.js
+++ src/toolbar/Toolbar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import './Toolbar.css';
-
+import {Link} from 'react-router-dom';
 const toolbar = props => (
 <header className="toolbar">
   <nav className="toolbar_navigation">
       <div></div>
-      <div className="toolbar_logo">
-        <a href="/">Vino's</a>
+      <div className="toolbar_logo">        
+        <Link to ='/'>Vino's</Link>
       </div>
       <div className="spacer"></div>
       <div className="toolbar_navigation_items">
           <ul>
-            <li>
-              <a href ="/CustList">Customers</a>          
+            <li>                      
+              <Link to ='/Custlist'>Customers</Link>
             </li>
-            <li>
-              <a href="/ViewList">Dog List</a>
+            <li>              
+              <Link to ='/ViewList'>Dog List</Link>
             </li>
           </ul>
       </div>
